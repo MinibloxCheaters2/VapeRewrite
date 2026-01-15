9356bd10df6f8717e92d3ece07771b443411d460
bab <bab@noreply.codeberg.org>
2026-01-15 15:13:27 -0600

    refactor(debugControls): oops! forgot to turn those back off...

--- src/debugControls.ts
+++ src/debugControls.ts
@@ -7,15 +7,15 @@
 
 //#region Store system
 /** Logs store names into the console. */
-export const LOG_STORE_NAME = true;
+export const LOG_STORE_NAME = false;
 /** Disables store name randomization. This makes Vape 20x easier to detect, so please don't toggle it. */
 export const NO_STORE_NAME_RANDOMIZATION = false;
 //#endregion
 //#region Replacement / dump system
 /** Checks for unmatched replacements, and if found, logs the unmatched replacements into the console */
-export const CHECK_UNMATCHED_REPLACEMENTS = true;
+export const CHECK_UNMATCHED_REPLACEMENTS = false;
 /** Checks for unmatched dumps, and if found, logs the unmatched dumps into the console. */
-export const CHECK_UNMATCHED_DUMPS = true;
+export const CHECK_UNMATCHED_DUMPS = false;
 /** Logs when we're applying replacements. */
 export const LOG_APPLYING_REPLACEMENTS = false;
 //#endregion
