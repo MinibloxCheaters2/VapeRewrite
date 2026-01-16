1972b7f1e0ec5a2f5cb07e488f142a40d49eb461
ProgMEMCC <progmem.cc@gmail.com>
2026-01-16 11:16:06 +0800

    feat(vape-rewrite) debug world

--- src/vape-rewrite/hooks/replacements.ts
+++ src/vape-rewrite/hooks/replacements.ts
@@ -2,6 +2,11 @@ import { storeName } from "../../Client";
 import { Replacement, Shift } from "./replacementTypes";
 
 // an interesting note, remove the type parameters (<string | RegExp, Replacement>) and then TypeScript starts complaining about types not being the same.
+const replacementWorldTypes = `availableWorldTypes = {
+		[GameModeId.SURVIVAL]: [WorldGenerationType.NORMAL, WorldGenerationType.SKYBLOCK, WorldGenerationType.ONEBLOCK, WorldGenerationType.FLAT, WorldGenerationType.VOID, WorldGenerationType.DEBUG],
+		[GameModeId.CREATIVE]: [WorldGenerationType.NORMAL, WorldGenerationType.FLAT, WorldGenerationType.VOID, WorldGenerationType.SKYBLOCK, WorldGenerationType.ONEBLOCK, WorldGenerationType.DEBUG],
+		[GameModeId.ADVENTURE]: [WorldGenerationType.NORMAL, WorldGenerationType.FLAT, WorldGenerationType.VOID, WorldGenerationType.SKYBLOCK, WorldGenerationType.ONEBLOCK, WorldGenerationType.DEBUG]
+	},`;
 
 export const REPLACEMENTS = new Map<string | RegExp, Replacement>(
   [
@@ -19,14 +24,16 @@ export const REPLACEMENTS = new Map<string | RegExp, Replacement>(
       shift: Shift.REPLACE,
     }],
 
-    ["new SPacketLoginStart({" +
+    [
+      "new SPacketLoginStart({" +
       "requestedUuid:localStorage.getItem(REQUESTED_UUID_KEY)??void 0," +
       'session:localStorage.getItem(SESSION_TOKEN_KEY)??"",' +
       'hydration:localStorage.getItem("hydration")??"0",' +
       'metricsId:localStorage.getItem("metrics_id")??"",' +
       "clientVersion:VERSION$1" +
-      "})", {
-      replacement: `new SPacketLoginStart({
+      "})",
+      {
+        replacement: `new SPacketLoginStart({
 requestedUuid: undefined,
 session: (window["${storeName}"].exposed.moduleManager.antiBan.enabled
 	? ""
@@ -35,8 +42,9 @@ hydration: "0",
 metricsId: uuid$1(),
 clientVersion: VERSION$1
 })`,
-      shift: Shift.REPLACE,
-    }],
+        shift: Shift.REPLACE,
+      },
+    ],
     ["static sendPacket(u){", {
       replacement:
         `window["${storeName}"].exposed.emitEvent("sendPacket", window["${storeName}"].exposed.newCancelableWrapper(u));`,
@@ -44,13 +52,23 @@ clientVersion: VERSION$1
     }],
     // enable all game modes
     [/index_browserExports\.useFlag("[^"]+")/g, {
-      replacement: 'true',
-      shift: Shift.REPLACE
+      replacement: "true",
+      shift: Shift.REPLACE,
     }],
     // Enable all ranks gifting
     ['jsxRuntimeExports.jsx("option",{value:"legend",children:"Legend"})', {
-      replacement: `,jsxRuntimeExports.jsx("option", { value: "immortal", children: "Immortal" })`,
-      shift: Shift.AFTER
-    }]
-  ]
+      replacement:
+        `,jsxRuntimeExports.jsx("option", { value: "immortal", children: "Immortal" })`,
+      shift: Shift.AFTER,
+    }],
+
+    // Enable Debug World Type
+    [/availableWorldTypes\s*=\s*\{[\s\S]*?\}\s*,/g, {
+      replacement: replacementWorldTypes,
+      shift: Shift.REPLACE,
+    }],
+
+
+    
+  ],
 );
