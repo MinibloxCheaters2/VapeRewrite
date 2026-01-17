032bdf3b6810295dff512de22a1cb9ba6e093925
bab <bab@noreply.codeberg.org>
2026-01-17 07:07:11 -0600

    refactor(Scaffold): changed category to blatant

--- src/vape-rewrite/features/module/impl/Scaffold.ts
+++ src/vape-rewrite/features/module/impl/Scaffold.ts
@@ -1,15 +1,8 @@
-import logger from "../../../utils/loggers";
 import Category from "../api/Category";
 import Mod from "../api/Module";
 
 export default class Scaffold extends Mod {
   public name = "Scaffold";
-  public category = Category.COMBAT;
-  public onEnable(): void {
-    logger.debug("Scaffold module enabled!");
-  }
-
-  public onDisable(): void {
-    logger.debug("Scaffold module disabled!");
-  }
+  public category = Category.BLATANT;
+  // TODO: scaffold module
 }
