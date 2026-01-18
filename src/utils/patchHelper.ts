dc10b0028d29a91e42be8e5f6bc073f763a05281
bab <bab@noreply.codeberg.org>
2026-01-18 13:46:20 -0600

    refactor(patchHelper): `as const`

--- src/utils/patchHelper.ts
+++ src/utils/patchHelper.ts
@@ -1,8 +1,8 @@
 import { storeName } from "../Client";
 import dump from "../hooks/dump";
 
-export const STORE = `window["${storeName}"]`;
-export const EXPOSED = `${STORE}.exposed`;
-export const MOD_MANAGER = `${EXPOSED}.moduleManager`;
+export const STORE = `window["${storeName}"]` as const;
+export const EXPOSED = `${STORE}.exposed` as const;
+export const MOD_MANAGER = `${EXPOSED}.moduleManager` as const;
 /** makes a string that accesses a dump */
 export function DMP<T extends keyof typeof dump>(n: T) { return `${EXPOSED}.dump.${n}` as const }
