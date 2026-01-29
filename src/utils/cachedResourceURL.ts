cdd008055ce097c1eef612f5bb196f11b8739fd9
bab <bab@noreply.codeberg.org>
2026-01-29 16:23:05 -0600

    docs(cachedResourceURL): useless changes

--- src/utils/cachedResourceURL.ts
+++ src/utils/cachedResourceURL.ts
@@ -1,13 +1,18 @@
-// stores resource URLs that are blob URLs
-const resourceURLCache: Record<string, string> = {};
+/**
+ * Why doesn't ViolentMonkey do this by default...
+ * @module
+ */
 
-// stores resource URLs that are data URLs
-const resourceURLCache2: Record<string, string> = {};
+/** Stores resource URLs that are blob URLs */
+const blobURLCache: Record<string, string> = {};
+
+/** Stores resource URLs that are data URLs */
+const dataURLCache: Record<string, string> = {};
 
 /**
  * @param name Name of a resource defined in the *Metadata Block*.
  * @param [isBlobUrl=true] use a `blob` URL?
- * otherwise use a `data` URL? Blob URLs short and cacheable,
+ * otherwise it will use a `data` URL. Blob URLs short and cacheable,
  * so blob URLs are good for reusing in multiple DOM elements.
  * It's long so reusing it in DOM may be less performant due to the lack of caching,
  * but it's particularly handy for direct synchronous decoding of the data on sites
@@ -17,7 +22,7 @@ export default function getResourceURL(
   name: string,
   isBlobUrl: boolean = true
 ) {
-	const cache = isBlobUrl ? resourceURLCache : resourceURLCache2;
+	const cache = isBlobUrl ? blobURLCache : dataURLCache;
 
 	cache[name] ??= GM_getResourceURL(name, isBlobUrl);
 
