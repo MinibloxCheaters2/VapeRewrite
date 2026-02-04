82c18a31bc770ffce2d2c04bd269ccffa2486149
bab <bab@noreply.codeberg.org>
2026-02-03 19:42:11 -0600

    refactor(Panic): you don't need a method in module manager to disable all modules

--- src/features/commands/impl/Panic.ts
+++ src/features/commands/impl/Panic.ts
@@ -1,12 +1,12 @@
-import { argument, literal } from "@wq2/brigadier-ts";
+import { literal } from "@wq2/brigadier-ts";
 import ModuleManager from "@/features/modules/api/ModuleManager";
-import ModuleArgumentType from "../api/brigadier/ModuleArgumentType";
 import dispatcher from "../api/CommandDispatcher";
 
 dispatcher.register(
-    literal("panic")
-        .executes(async () => {
-            ModuleManager.disableAll();
-            return 1;
-        })
+	literal("panic").executes(async () => {
+		ModuleManager.modules.forEach((m) => {
+			m.enabled = false;
+		});
+		return 1;
+	}),
 );
