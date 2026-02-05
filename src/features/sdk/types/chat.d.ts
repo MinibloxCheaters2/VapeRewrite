28d2dcaba2c2b087b414468b3c492a1020632cdd
bab <bab@noreply.codeberg.org>
2026-02-04 23:02:54 -0600

    refactor(Chat): I think it's fully typed now

--- src/features/sdk/types/chat.d.ts
+++ src/features/sdk/types/chat.d.ts
@@ -30,7 +30,17 @@ export declare class Chat {
 	showInput: boolean;
 	isInputCommandMode: boolean;
 	setAutoCompleteDefault(): void;
+	/** `idk` is only passed to `ClientSideCommands.tryExecuteClientSide` */
+	submit(idk: unknown): void;
+	getHistory(direction: "up" | "down"): void;
+	setInputValue(value: string): void;
+	openInput(commandMode: boolean): void;
+	closeInput(): void;
+	filterUsernames(data: ChatData): ChatData;
 	addChat(data: ChatData): void;
-	autoCompleteReceived(packet: CPacketTabComplete): void;
 	clear(): void;
+	requestTabComplete(updateMoveBackwards: boolean): void;
+	autoCompleteReceived(packet: CPacketTabComplete): void;
+	updateInputWithAutoComplete(moveBackwards: boolean): void;
+	static trimLastWord(input: string): string;
 }
