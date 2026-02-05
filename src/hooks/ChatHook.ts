69479043709407a59746f06b3879a3ba83c6c76e
bab <bab@noreply.codeberg.org>
2026-02-04 23:17:51 -0600

    refactor(ChatHook): add modifyChat & modifyChatAppend

--- src/hooks/ChatHook.ts
+++ src/hooks/ChatHook.ts
@@ -3,7 +3,7 @@
  * @module
  */
 
-import type { Chat, ChatData } from "@/features/sdk/types/chat";
+import type { Chat, ChatData, ChatLog } from "@/features/sdk/types/chat";
 import Refs from "@/utils/refs";
 
 const idSymbol = Symbol();
@@ -12,8 +12,9 @@ export type UUIDv4 = `${string}-${string}-4${string}-${string}-${string}`;
 
 export default new (class ChatHook {
 	origAddChat: Chat["addChat"];
+
 	constructor() {
-		const { chat } = Refs.game;
+		const { chat } = Refs;
 		this.origAddChat = chat.addChat;
 		chat.addChat = new Proxy(chat.addChat, {
 			apply(orig, ts, args: [ChatData]) {
@@ -25,5 +26,32 @@ export default new (class ChatHook {
 				return orig;
 			},
 		});
+	}
+	modifyChat(
+		id: UUIDv4,
+		modifier: ChatLog | ((old: ChatLog) => ChatLog),
+	): boolean {
+		const { log } = Refs.chat;
+		const origIdx = log.findIndex((log) => log[idSymbol] === id);
+		if (origIdx === -1) return false;
+		const orig = log[origIdx];
+		const modified =
+			typeof modifier === "function" ? modifier(orig) : modifier;
+		log[origIdx] = modified;
+		return true;
+	}
+	modifyChatAppend(
+		id: UUIDv4,
+		modifier: ChatLog | ((old: ChatLog) => ChatLog),
+	): boolean {
+		const { log } = Refs.chat;
+		const origIdx = log.findIndex((log) => log[idSymbol] === id);
+		if (origIdx === -1) return false;
+		const orig = log[origIdx];
+		const modified =
+			typeof modifier === "function" ? modifier(orig) : modifier;
+		delete log[origIdx];
+		log.push(modified);
+		return true;
 	}
 })();
