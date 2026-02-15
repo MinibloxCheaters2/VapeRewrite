07d40469a18bcb2812724383066fd205a4282f52
TheM1ddleM1n <192749026+TheM1ddleM1n@users.noreply.github.com>
2026-02-15 18:29:36 +0000

    Update AutoSword.ts
    now??

--- src/features/modules/impl/utility/AutoSword.ts
+++ src/features/modules/impl/utility/AutoSword.ts
@@ -11,10 +11,10 @@ export default class AutoSword extends Mod {
 	private rangeSetting = this.createSliderSetting("Range", 10, 3, 20, 0.5);
 	private swapBackSetting = this.createToggleSetting("Swap Back", true);
 	private prioritySetting = this.createDropdownSetting(
-	"Priority",
-	["Damage", "Durability"],
-	"Damage",
-   );
+		"Priority",
+		["Damage", "Durability"],
+		"Damage",
+	);
 
 	private previousSlot: number | null = null;
 	private lastSlotSwitch = 0;
