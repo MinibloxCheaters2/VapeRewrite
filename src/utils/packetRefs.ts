00507e88d368652400c3127b38c3505bb5de9192
bab <bab@noreply.codeberg.org>
2026-02-23 19:07:29 -0600

    a

--- src/utils/packetRefs.ts
+++ src/utils/packetRefs.ts
@@ -1,3 +1,7 @@
+/**
+ * contains a cache to packet references.
+ * @module
+ */
 import Interop from "../exposedO";
 import type { CPACKET_MAP, SPACKET_MAP } from "../features/sdk/types/packets";
 
@@ -5,35 +9,91 @@ export type CPacketMap = typeof CPACKET_MAP;
 export type SPacketMap = typeof SPACKET_MAP;
 export type AnyPacketMap = CPacketMap & SPacketMap;
 
-const PacketRefs = {
-	cRefs: {} as CPacketMap,
-	sRefs: {} as SPacketMap,
-};
+/** todo: supporting abbreviated names? i.e. PacketRefs.s.SPacketSomething -> PacketRefs.s.Something */
+function makeProxyRef<T extends CPacketMap | SPacketMap, V = T[keyof T]>(
+	obj: T,
+	getUncached: (k: string | symbol) => V,
+) {
+	return new Proxy<T>(obj, {
+		get(target, v) {
+			const orig = Reflect.get(target, v) as V;
+			let r = orig;
+			if (!orig) {
+				r = getUncached(v);
+				Reflect.set(target, v, r);
+			}
+			return r;
+		},
+	});
+}
 
+function getC2SUncached<
+	K extends keyof SPacketMap,
+	V extends SPacketMap[K] = SPacketMap[K],
+>(ref: K): V {
+	if (typeof ref === "symbol") {
+		throw "can't get a c2s packet with a name that is a symbol instead of a string.";
+	}
+	return Interop.run((evl) => {
+		const pkt = evl<V>(ref);
+		PacketRefs.c[ref as string] = pkt;
+		return pkt;
+	});
+}
+
+function getS2CUncached<
+	K extends keyof CPacketMap,
+	V extends CPacketMap[K] = CPacketMap[K],
+>(ref: K): V {
+	if (typeof ref === "symbol") {
+		throw "can't get a c2s packet with a name that is a symbol instead of a string.";
+	}
+	return Interop.run((evl) => {
+		const pkt = evl<V>(ref);
+		PacketRefs.c[ref] = pkt;
+		return pkt;
+	});
+}
+
+/**
+ * note: use `PacketRefs.c` instead when trying to do `new {packet}`, it's way cleaner since instead of:
+ * ```ts
+ * const pkt = new (c2s("SPacketSomething"));
+ * ```
+ * you can do
+ * ```ts
+ * const pkt = new PacketRefs.s.SPacketSomething;
+ * ```
+ */
 export function c2s<
 	K extends keyof SPacketMap,
 	V extends SPacketMap[K] = SPacketMap[K],
 >(ref: K): V {
-	if (PacketRefs.sRefs[ref] === undefined) {
-		return Interop.run((evl) => {
-			const pkt = evl<V>(ref as string);
-			PacketRefs.cRefs[ref as string] = pkt;
-			return pkt;
-		});
-	}
-	return PacketRefs.cRefs[ref as string] as V;
+	return PacketRefs.s[ref as string] as V;
 }
 
+/**
+ * note: use `PacketRefs.s` instead when trying to do `new {packet}`, it's way cleaner since instead of:
+ * ```ts
+ * const pkt = new (s2c("CPacketSomething"));
+ * ```
+ * you can do
+ * ```ts
+ * const pkt = new PacketRefs.c.CPacketSomething;
+ * ```
+ */
 export function s2c<
 	K extends keyof CPacketMap,
 	V extends CPacketMap[K] = CPacketMap[K],
 >(ref: K): V {
-	if (PacketRefs.cRefs[ref] === undefined) {
-		return Interop.run((evl) => {
-			const pkt = evl<V>(ref as string);
-			PacketRefs.cRefs[ref] = pkt;
-			return pkt;
-		});
-	}
-	return PacketRefs.cRefs[ref] as V;
+	return PacketRefs.c[ref] as V;
 }
+
+const PacketRefs = {
+	/** Client -> Server packets */
+	c: makeProxyRef({} as CPacketMap, getC2SUncached),
+	/** Server -> Client packets */
+	s: makeProxyRef({} as SPacketMap, getS2CUncached),
+};
+
+export default PacketRefs;
