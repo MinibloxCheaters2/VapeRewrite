38257cfaaa94282cd188b0664ef43dc500cc6051
bab <bab@noreply.codeberg.org>
2026-02-28 16:33:00 -0600

    a

---
+++ rolldown.config.ts
@@ -0,0 +1,53 @@
+import { isAbsolute, relative, resolve } from "node:path";
+import { readPackageUp } from "read-package-up";
+import { defineConfig, ExternalOption } from "rolldown";
+import userscript from "rolldown-plugin-userscript";
+
+const { packageJson } = (await readPackageUp())!;
+
+export const BUILD_DATE = new Date();
+
+// on April 1st, Baby Oil Rewrite. otherwise, Vape Rewrite.
+export const REAL_CLIENT_NAME =
+	BUILD_DATE.getMonth() === 3 && BUILD_DATE.getDate() === 1
+		? ("Baby Oil Rewrite" as const)
+		: ("Vape Rewrite" as const);
+
+export default defineConfig(
+	{
+		input: "src/index.ts",
+		plugins: [
+			userscript((meta: string) => {
+				const newMeta = meta
+					.replace("process.env.AUTHOR", packageJson.author?.name ?? "Unspecified")
+					.replace("process.env.VERSION", packageJson.version)
+					.replace("process.env.NAME", REAL_CLIENT_NAME);
+				return newMeta;
+			}),
+		],
+		external: defineExternal(["@violentmonkey/dom"]),
+		output: {
+			format: "iife",
+			file: `dist/vape-rewrite.user.js`,
+			globals: {
+				"@violentmonkey/dom": "VM",
+			},
+			minify: process.env.NODE_ENV === "production",
+			sourcemap: "inline",
+		},
+	}
+);
+
+function defineExternal(externals: ((string | ((id: string) => boolean)) | RegExp)[]): ExternalOption {
+	return (id) =>
+		externals.some((pattern) => {
+			if (typeof pattern === "function") return pattern(id);
+			if (pattern instanceof RegExp) {
+				return pattern.test(id);
+			}
+			if (isAbsolute(pattern)) {
+				return !relative(pattern, resolve(id)).startsWith("..");
+			}
+			return id === pattern || id.startsWith(pattern + "/");
+		});
+}
