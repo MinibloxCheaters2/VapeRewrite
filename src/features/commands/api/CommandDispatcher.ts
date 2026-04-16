4940f7bfb035e4947f925a233845a8dc0c099176
bab <bab@noreply.codeberg.org>
2026-04-15 22:11:01 -0500

    refactor(CommandDispatcher): eeeeee

--- src/features/commands/api/CommandDispatcher.ts
+++ src/features/commands/api/CommandDispatcher.ts
@@ -1,7 +1,7 @@
 import { CommandDispatcher } from "@wq2/brigadier-ts";
 
-export class CommandSource {
-	// TODO: idk if we need any things here, but probably not.
-}
+// export class CommandSource {
+// 	// idk if we need any things here, but probably not.
+// }
 
-export default new CommandDispatcher<CommandSource>();
+export default new CommandDispatcher<unknown>();
