3fe58cf01f4b0606738886cb495a1af5531586d5
bab <bab@noreply.codeberg.org>
2026-07-20 14:50:30 -0500

    another attempt

--- packetGenWrapper.js
+++ packetGenWrapper.js
@@ -1,5 +1,8 @@
-import("https://cdn.jsdelivr.net/npm/@wq2/packet-gen-wasm@0.0.0/packet_gen_wasm.min.js").then(
-	(x) => {
-		window.VM = x;
-	},
-);
+// packetGenWrapper.js
+import init, * as wasmExports from "https://cdn.jsdelivr.net/npm/@wq2/packet-gen-wasm@0.0.0/packet_gen_wasm.js";
+
+init(
+	"https://cdn.jsdelivr.net/npm/@wq2/packet-gen-wasm@0.0.0/packet_gen_wasm_bg.wasm",
+).then(() => {
+	window.WASM = wasmExports;
+});
