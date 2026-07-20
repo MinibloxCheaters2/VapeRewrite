526105b5f5e7050b5e297d359dcfa74ac219e0f4
bab <bab@noreply.codeberg.org>
2026-07-20 14:36:20 -0500

    d

--- src/utils/network/WasmTest.ts
+++ src/utils/network/WasmTest.ts
@@ -4,7 +4,7 @@ import logger from "../logging/loggers";
 
 async function init() {
 	await gameScriptReady;
-	const result = parse(gameScript);
+	const result = VM.parse(gameScript);
 	logger.info("Parsed game script", result);
 	return result;
 }
