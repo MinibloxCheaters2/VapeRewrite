3b633d5805decf6bcfa0e2cecc66076b31bb6054
bab <bab@noreply.codeberg.org>
2026-07-20 14:28:22 -0500

    draft: trying to test a thingy [skip ci]

---
+++ src/utils/network/WasmTest.ts
@@ -0,0 +1,12 @@
+import { gameScript, gameScriptReady } from "@/hooks/gameScript";
+//import { parse } from "@wq2/packet-gen-wasm";
+import logger from "../logging/loggers";
+
+async function init() {
+	await gameScriptReady;
+	const result = parse(gameScript);
+	logger.info("Parsed game script", result);
+	return result;
+}
+
+export const ready = init();
