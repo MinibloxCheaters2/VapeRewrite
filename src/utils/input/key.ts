42f1f9c1ce7f44911bc7a2d3ece30066de726fbf
bab <bab@noreply.codeberg.org>
2026-07-22 23:07:28 -0500

    fix(key): inverted the pressedKeys thing lmao

--- src/utils/input/key.ts
+++ src/utils/input/key.ts
@@ -2,8 +2,8 @@ import Miniblox from "../refs/miniblox";
 
 const pressedKeys: Record<string, boolean> = {};
 
-document.addEventListener("keydown", (x) => (pressedKeys[x.code] = false));
-document.addEventListener("keyup", (x) => (pressedKeys[x.code] = true));
+document.addEventListener("keydown", (x) => (pressedKeys[x.code] = true));
+document.addEventListener("keyup", (x) => (pressedKeys[x.code] = false));
 
 export default function isKeyDown(key: string): boolean {
 	const { Game } = Miniblox;
