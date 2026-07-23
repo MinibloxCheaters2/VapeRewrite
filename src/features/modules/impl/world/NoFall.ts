dc464b9c9721b69e23570125b55c8406206b4fd4
bab <bab@noreply.codeberg.org>
2026-07-23 13:20:06 -0500

    feat(NoFall): Normal mode (best, especially for maces)
    Normal mode doesn't modify your fall distance, it just makes it so fall
    damage doesn't apply when you hit the ground
    see:
    <https://github.com/CCBlueX/LiquidBounce/blob/nextgen/src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallHypixel.kt>
    this also works on mc for 1.17+ servers, but it also works for Miniblox,
    and arguably better than ground spoofing.

--- src/features/modules/impl/world/NoFall.ts
+++ src/features/modules/impl/world/NoFall.ts
@@ -20,20 +20,42 @@ const MAX_FALL_HEIGHT = 3;
 export default class NoFall extends Mod {
 	public name = "NoFall";
 	public category = Category.WORLD;
+	#modeSetting = this.createDropdownSetting("Mode", ["Normal", "Ground Spoof"]);
+	#doJump = false;
+
+	private get mode() {
+		return this.#modeSetting.value();
+	}
 
 	protected onEnable(): void {}
 
 	@Subscribe("sendPacket")
 	onPacket({ data: packet }: CancelableWrapper<C2SPacket>) {
 		if (
-			isC2S("SPacketPlayerPosLook", packet) &&
-			!packet.onGround &&
-			!ModuleManager.maceKill.hackyFallDamageFix &&
-			PacketFallDistance.currentFallDistance >=
-				/*Miniblox.player.getMaxFallHeight()*/MAX_FALL_HEIGHT - FALL_HEIGHT_BUFFER
+			isC2S("SPacketPlayerPosLook", packet)
 		) {
-			packet.onGround = true;
-			Miniblox.player.fallDistance = 0.0;
+			switch (this.mode) {
+				// https://github.com/CCBlueX/LiquidBounce/blob/nextgen/src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallHypixel.kt
+				// works best with maces and other stuff that requires your fall distance to be accurate
+				case "Normal": {
+					if (PacketFallDistance.currentFallDistance >= /*Miniblox.player.getMaxFallHeight()*/MAX_FALL_HEIGHT - FALL_HEIGHT_BUFFER) this.#doJump = true;
+					if (this.#doJump) {
+						const { player } = Miniblox;
+						if (!player.onGround) return;
+						packet.onGround = false;
+						player.pos.setY(player.pos.y + 0.09);
+						this.#doJump = false;
+					}
+				}
+					break;
+				case "Ground Spoof": {
+					if (!packet.onGround && PacketFallDistance.currentFallDistance >= /*Miniblox.player.getMaxFallHeight()*/MAX_FALL_HEIGHT - FALL_HEIGHT_BUFFER) {
+						packet.onGround = true;
+						Miniblox.player.fallDistance = 0;
+					}
+					break;
+				}
+			}
 		}
 	}
 }
