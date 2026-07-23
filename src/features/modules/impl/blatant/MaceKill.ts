7cd1ed547515965aeb3b424a85cceb2c1ee4afa3
bab <bab@noreply.codeberg.org>
2026-07-22 22:59:20 -0500

    fix(MaceKill): FINALLY IT SLIGHTLY WORKS

--- src/features/modules/impl/blatant/MaceKill.ts
+++ src/features/modules/impl/blatant/MaceKill.ts
@@ -1,16 +1,15 @@
+import type { C2SPacket } from "@wq2/miniblox-sdk";
 import { Subscribe } from "@/event/Bus";
+import type CancelableWrapper from "@/event/CancelableWrapper";
+import { isC2S, sendSilently } from "@/utils";
+import PacketRefs from "@/utils/network/packetRefs";
+import Miniblox from "@/utils/refs/miniblox";
 import Category from "../../api/Category";
 import Mod from "../../api/Module";
-import CancelableWrapper from "@/event/CancelableWrapper";
-import { C2SPacket } from "@wq2/miniblox-sdk";
-import { isC2S, MAIN_LOGGER as logger } from "@/utils";
-import Miniblox from "@/utils/refs/miniblox";
-import PacketRefs from "@/utils/network/packetRefs";
-import ServerFallDistance from "@/utils/movement/ServerFallDistance";
 
 function teleport(y: number, onGround: boolean) {
-	const { ClientSocket, player } = Miniblox;
-	ClientSocket.sendPacket(
+	const { player } = Miniblox;
+	sendSilently(
 		new PacketRefs.s.SPacketPlayerPosLook({
 			pos: {
 				x: player.pos.x,
@@ -20,34 +19,18 @@ function teleport(y: number, onGround: boolean) {
 			onGround,
 		}),
 	);
-	if (PacketRefs.s.SPacketPlayerInput) ClientSocket.sendPacket(
-		new PacketRefs.s.SPacketPlayerInput({
-			pos: {
-				x: player.pos.x,
-				y,
-				z: player.pos.z,
-			},
-			sequenceNumber: player.inputSequenceNumber++,
-			yaw: player.yaw,
-			pitch: player.pitch,
-			jump: y > player.pos.y ? true : false,
-			down: false,
-			left: false,
-			right: false,
-			sneak: false,
-			sprint: player.isSprinting(),
-			up: false,
-			//@ts-expect-error: it does, just outdated packet def
-			onGround,
-		}),
-	);
 }
 
 export default class MaceKill extends Mod {
 	public name = "MaceKill";
 	public category = Category.BLATANT;
 
-	#fallDistance = this.createSliderSetting("FallDistance", 100, 15, 1_000_000);
+	#fallDistance = this.createSliderSetting(
+		"FallDistance",
+		100,
+		15,
+		1_000_000,
+	);
 
 	get fallDistance() {
 		return this.#fallDistance.value();
@@ -56,10 +39,10 @@ export default class MaceKill extends Mod {
 	@Subscribe("sendPacket")
 	onSendPacket({ data: pkt }: CancelableWrapper<C2SPacket>) {
 		if (!isC2S("SPacketUseEntity", pkt) || pkt.action !== 1) return;
-		const { ClientSocket, player, Items } = Miniblox;
+		const { player, Items } = Miniblox;
 		if (player.inventory.getCurrentItem()?.item !== Items.mace) return;
 		teleport(player.pos.y + this.fallDistance, false);
-		teleport(player.pos.y, false);
-		player.fallDistance = ServerFallDistance.currentFallDistance;
+		teleport(player.pos.y + (this.fallDistance - 0.08), false);
+		teleport(player.pos.y + 1, false);
 	}
 }
